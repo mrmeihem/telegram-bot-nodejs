6ac27b90d8d47c72b0e9848bded989fc3e5fc0f5
Mikhail Karpenko <70774954+mrmeihem@users.noreply.github.com>
2022-11-26 22:00:43 +0300

    TBot + node cron test. No success.

---
+++ const.js
@@ -0,0 +1,8 @@
+// Команды бота
+const commands = `
+/start - Перезапустить бота
+/help - Помощь
+/chron - Отсчет от 10 до 1 
+`
+// Экспорт констант
+module.exports.commands = commands
--- index.js
+++ index.js
@@ -1,3 +1,58 @@
-// run `node index.js` in the terminal
+// Импорт Telegraf и Markup
+const {Telegraf, Markup} = require('telegraf')
+// Импорт dotenv для защиты API токена
+require('dotenv').config()
+// Импорт хрона
+const cron = require('node-cron');
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+// Импорт модуля с константами
+const my_const = require('./const')
+
+let i = 0;
+
+const task = cron.schedule('* * * * * *', () => {
+  console.log('stopped task-' + i);
+  i++;
+}, {
+  scheduled: false
+});
+task.start();
+
+// Инициализация бота с помощью Telegraf
+const bot = new Telegraf(process.env.BOT_TOKEN)
+
+// Обработка команды /start
+bot.start((ctx) => ctx.reply(`Привет ${ctx.message.from.first_name ? ctx.message.from.first_name : 'незнакомец'}!`))
+// Обработка команды /help
+bot.help((ctx) => ctx.reply(my_const.commands))
+// Обработка команды /course
+bot.command('chron', async (ctx) => {
+  try {
+    await ctx.replyWithHTML('<b>Countdown</b>', Markup.inlineKeyboard(
+      [Markup.button.callback('Start', 'btn_c')]
+    ))
+  } catch (e) {
+    console.error(e)
+  }
+})
+
+const addActionBot = (id_btn) => {
+  bot.action(id_btn, async (ctx) => {
+    try {
+      await ctx.answerCbQuery();
+      ctx.reply(`AlЯrm!`);
+    } catch (e) {
+      console.error(e)
+    }
+  })
+}
+
+// Обработчик кнопок с помощью функции
+addActionBot('btn_c')
+
+// Запустить бота
+bot.launch()
+
+// Включить плавную остановку
+process.once('SIGINT', () => bot.stop('SIGINT'))
+process.once('SIGTERM', () => bot.stop('SIGTERM'))
